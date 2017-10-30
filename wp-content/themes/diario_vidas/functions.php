1b981a0fb61e299c50c3ceacca20ed2360345fdc
Francisca Medina Concha <fbmedina@uc.cl>
2017-10-29 22:38:29 -0300

    sacado error

--- wp-content/themes/diario_vidas/functions.php
+++ wp-content/themes/diario_vidas/functions.php
@@ -7,13 +7,6 @@
 			require_once $file;
 		}
 	}
-
-	function dl_enqueue_style() {
-		wp_register_style('reset', get_theme_file_uri() . '/assets/css/reset.css', null, null, null);
-		wp_register_style('mainStyle', get_theme_file_uri() . '/assets/css/style.css', null, null, null);
-		wp_enqueue_style('reset');
-		wp_enqueue_style('mainStyle');
-	}
 	
 	add_action( 'wp_enqueue_scripts', 'dl_enqueue_style' );
 

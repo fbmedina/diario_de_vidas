3cb93c4844645660b7f717844b9e2b448ce60275
Francisca Medina Concha <fbmedina@uc.cl>
2017-11-01 18:53:54 -0300

    configuring css screens

--- wp-content/themes/diario_vidas/functions/wp_enqueue_style.php
+++ wp-content/themes/diario_vidas/functions/wp_enqueue_style.php
@@ -5,10 +5,16 @@
 
 		/* Register Scripts */
 		wp_register_style('reset', get_parent_theme_file_uri('/assets/css/reset.css'), null, $theme_data->get( 'Version' ), 'screen');
-		wp_register_style('mainStyle', get_parent_theme_file_uri('/assets/css/style.css'), array('reset'), $theme_data->get( 'Version' ), 'screen');
+		wp_register_style('mainStyle', get_parent_theme_file_uri('/assets/css/main.css'), array('reset'), $theme_data->get( 'Version' ), 'screen');
+		wp_register_style('extraSmallScreen', get_theme_file_uri() . '/assets/css/screen-xs.css', null, null, null);
+		wp_register_style('smallScreen', get_theme_file_uri() . '/assets/css/screen-sm.css', array('extraSmallScreen'), null, null);
+		wp_register_style('mediumScreen', get_theme_file_uri() . '/assets/css/screen-md.css', array('smallScreen'), null, null);
+		wp_register_style('largeScreen', get_theme_file_uri() . '/assets/css/screen-lg.css', array('mediumScreen'), null, null);
+		wp_register_style('extraLargeScreen', get_theme_file_uri() . '/assets/css/screen-xl.css', array('largeScreen'), null, null);
 		
 		/* Enqueue Scripts */
 		wp_enqueue_style('mainStyle');
+		wp_enqueue_style('extraLargeScreen');
 	}
 
 	add_action( 'wp_enqueue_scripts', 'dl_enqueue_style' );

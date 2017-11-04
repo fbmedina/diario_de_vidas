7e4a5f9f013e51f3b11e7c482c28dc8b89fc3b8e
Francisca Medina Concha <fbmedina@uc.cl>
2017-11-04 15:49:55 -0300

    image size functions

--- wp-content/themes/diario_vidas/functions/add_image_sizes.php
+++ wp-content/themes/diario_vidas/functions/add_image_sizes.php
@@ -1,20 +1,16 @@
 <?php
 	
-	// Agrega los tamaños personalizados en la lista de tamaños disponibles en el administrador
-	function dl_image_sizes($sizes) {
+	function dl_image_sizes( $sizes ) {
 		$addsizes = array(
-			"slideshow" => __( "Tamaño del slideshow"),
-			"custom_logo" => __( "Tamaño personalizado del logo")
+			"member" => __( "Tamaño del la imagen principal del miembro del equipo"),
+			"story_img" => __( "Tamaño del las imágenes de los relatos")
 		);
-
 		return array_merge($sizes, $addsizes);
 	}
 
-	// Agrega tamaños de imágenes personalizadas en posts
-	if ( function_exists( 'add_theme_support' ) ) { 
-		add_image_size( 'slideshow', 800, 600, true);		// Personalización del tamaño del slideshow
-		add_image_size( 'custom_logo', 800, 600, true);		// Personalización del tamaño del logo
-
+	if ( function_exists( 'add_theme_support' ) ) {
+		add_image_size( 'member', 600, 600, true);
+		add_image_size( 'story_img', 600);
 		add_filter('image_size_names_choose', 'dl_image_sizes');
 	}
 

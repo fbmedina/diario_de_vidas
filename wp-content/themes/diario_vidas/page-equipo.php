25e44e3d0fc82d2e1f0a3b6292ab80c90adff0c6
Francisca Medina Concha <fbmedina@uc.cl>
2017-11-04 11:27:26 -0300

    created page-equipo

---
+++ wp-content/themes/diario_vidas/page-equipo.php
@@ -0,0 +1,17 @@
+<?php get_header() ?>
+
+<main>
+	<section class="landing">
+		<h1>Diario de Vidas</h1>
+		<div class="main_description">
+			<p>Autobiografía y relatos de vida para el apoyo a la diversidad sexual adolescente.</p>
+		</div>
+	</section>
+	<section class="remarkable">
+		<h1>Diario de Vidas</h1>
+	</section>
+	<section class="content">
+	</section>
+</main>
+
+<?php get_footer() ?>

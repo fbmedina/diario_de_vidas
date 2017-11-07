811f5baabbf978f962bb6dee6a43a91495ab9b91
Francisca Medina Concha <fbmedina@uc.cl>
2017-11-07 16:50:42 -0300

    page equipo styled

--- wp-content/themes/diario_vidas/page-equipo.php
+++ wp-content/themes/diario_vidas/page-equipo.php
@@ -3,11 +3,10 @@
 	<section class="title">
 		<h2><?php the_title() ?></h2>
 	</section>
-	<section class="content">
 		<?php
 			$arg = array(
-				'post_type'		 => 'member',
-				'posts_per_page' => -1
+				'post_type'			=> 'member',
+				'posts_per_page'	=> -1
 			);
 
 			$get_arg = new WP_Query( $arg );
@@ -15,19 +14,17 @@
 			while ( $get_arg->have_posts() ) {
 				$get_arg->the_post();
 			?>
-				<article>
-			     	<div>
-			     		<?php the_post_thumbnail('member'); ?>
-			     	</div>
-			     	<div>
-			     		<h3 class="member_name"><?php the_title() ?></h3>
-			     		<h4><?php echo get_post_meta($post->ID, 'Rol', true); ?></h4>
-			     		<p><?php the_content() ?></p>
-			     	</div>
-			    </article>
+		     	<section class="half-left">
+		     		<?php the_post_thumbnail('member'); ?>
+		     	</section>
+
+		     	<section class="half-right">
+		     		<h3 class="member_name"><?php the_title() ?></h3>
+		     		<h4 class="rol"><?php echo get_post_meta($post->ID, 'Rol', true); ?></h4>
+		     		<p><?php the_content() ?></p>
+		     	</section>
 		    <?php } wp_reset_postdata();
 		?>
-	</section>
 </main>
 
 <?php get_footer() ?>

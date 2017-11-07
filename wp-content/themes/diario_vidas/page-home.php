45dc1f81d2dcf1c40a5e2cb8066e726cf39be45c
Francisca Medina Concha <fbmedina@uc.cl>
2017-11-07 16:31:40 -0300

    new classes for sections in home page

--- wp-content/themes/diario_vidas/page-home.php
+++ wp-content/themes/diario_vidas/page-home.php
@@ -3,24 +3,27 @@
 	<section class="title">
 		<h2><?php the_title() ?></h2>
 	</section>
-	<section class="content">
-		<div>
-			<p><?php the_field('que_es_ddv_1'); ?></p>
-		</div>
-		<div>
-			<p class="significant align_right">:Ejemplo de foto</p>
-			<?php $image = get_field('imagen_1'); ?>
-    		<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
-		</div>
-		<div>
-			<p class="significant">Ejemplo de video:</p>
-			<?php the_field('video_de_ejemplo'); ?>
-		</div>
-		<div>
-			<p><?php the_field('que_es_ddv_2'); ?></p>
-		</div>
+
+	<section class="half-left">
+		<p><?php the_field('que_es_ddv_1'); ?></p>
 	</section>
-	<section class="relato_example">
+
+	<section class="half-right">
+		<p class="significant align_right">:Ejemplo de foto</p>
+		<?php $image = get_field('imagen_1'); ?>
+		<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+	</section>
+
+	<section class="half-left">
+		<p class="significant">Ejemplo de video:</p>
+		<?php the_field('video_de_ejemplo'); ?>
+	</section>
+
+	<section class="half-right">	
+		<p><?php the_field('que_es_ddv_2'); ?></p>
+	</section>
+
+	<section class="full">
 		<p>El siguiente es un ejemplo de relato real enviado anónimamente:</p>
 		<p class="significant"><?php the_field('ejemplo_relato'); ?></p>
 	</section>

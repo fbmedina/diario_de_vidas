21981e362c5947f780237465e750d3e673ae3f2e
Francisca Medina Concha <fbmedina@uc.cl>
2017-11-07 19:45:43 -0300

    relato style

--- wp-content/themes/diario_vidas/page-relatos.php
+++ wp-content/themes/diario_vidas/page-relatos.php
@@ -3,53 +3,51 @@
 	<section class="title">
 		<h2><?php the_title() ?></h2>
 	</section>
-	<section class="content">
-		<?php
-			$arg = array(
-				'post_type'		 => 'story',
-				'posts_per_page' => -1
-			);
+	<?php
+		$arg = array(
+			'post_type'		 => 'story',
+			'posts_per_page' => -1
+		);
 
-			$get_arg = new WP_Query( $arg );
+		$get_arg = new WP_Query( $arg );
 
-			while ( $get_arg->have_posts() ) {
-				$get_arg->the_post();
-			?>
-				<article>
-			     	<div>
-			     		<h3 class="story_title"><?php the_title() ?></h3>
-			     		<h4 class="story_author"><?php echo get_post_meta($post->ID, 'Autor', true); ?>.</h4>
-			     	</div>
-			     	<div>
-			     		<p><?php the_content() ?></p>
-			     	</div>
-			     	<div>
-			     		<div>
-			     			<?php $image = get_field('imagen_1'); ?>
-    							<img src="<?php echo $image['url']; ?>">
-    					</div>
-
-    					<?php if (get_field('imagen_2')): ?>
-    						<div>
-	    						<?php $image2 = get_field('imagen_2'); ?>
-	    							<img src="<?php echo $image2['url']; ?>">
-	    					</div>
-    					<?php endif ?>
-
-    					<?php if (get_field('imagen_3')): ?>
-    						<div>
-	    						<?php $image3 = get_field('imagen_3'); ?>
-	    							<img src="<?php echo $image3['url']; ?>">
-	    					</div>
-    					<?php endif ?>
-			     	</div>
-			     	<div>
-			     		<p class="story_tags"><?php the_tags( '#', ' #', '' ); ?></p>
-			     	</div>
-			    </article>
-		    <?php } wp_reset_postdata();
+		while ( $get_arg->have_posts() ) {
+			$get_arg->the_post();
 		?>
-	</section>
+			<article class="full relato">
+		     	<div class="about">
+		     		<h3 class="story_title"><?php the_title() ?></h3>
+		     		<h4 class="story_author">por: <?php echo get_post_meta($post->ID, 'Autor', true); ?>.</h4>
+		     	</div>
+		     	<div class="content">
+		     		<p><?php the_content() ?></p>
+		     	</div>
+		     	<div class="multimedia">
+		     		<div>
+		     			<?php $image = get_field('imagen_1'); ?>
+							<img src="<?php echo $image['url']; ?>">
+					</div>
+
+					<?php if (get_field('imagen_2')): ?>
+						<div>
+    						<?php $image2 = get_field('imagen_2'); ?>
+    							<img src="<?php echo $image2['url']; ?>">
+    					</div>
+					<?php endif ?>
+
+					<?php if (get_field('imagen_3')): ?>
+						<div>
+    						<?php $image3 = get_field('imagen_3'); ?>
+    							<img src="<?php echo $image3['url']; ?>">
+    					</div>
+					<?php endif ?>
+		     	</div>
+		     	<div class="hashtags">
+		     		<p class="story_tags"><?php the_tags( '#', ' #', '' ); ?></p>
+		     	</div>
+		    </article>
+	    <?php } wp_reset_postdata();
+	?>
 </main>
 
 <?php get_footer() ?>

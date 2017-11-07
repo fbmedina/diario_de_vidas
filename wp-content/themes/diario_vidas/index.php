696e3f027c6b7e89cbd31db15df3221743aac120
Francisca Medina Concha <fbmedina@uc.cl>
2017-11-07 15:44:16 -0300

    index page without content

--- wp-content/themes/diario_vidas/index.php
+++ wp-content/themes/diario_vidas/index.php
@@ -1,36 +1,3 @@
 <?php get_header() ?>
 
-<main>
-	<section class="landing">
-		<h1>Diario de Vidas</h1>
-		<div class="main_description">
-			<p>Autobiografía y relatos de vida para el apoyo a la diversidad sexual adolescente.</p>
-		</div>
-	</section>
-	<section class="remarkable">
-		<h1>Diario de Vidas</h1>
-	</section>
-	<section class="content">
-		<h2>Qué es DdV</h2>
-		<div>
-			<p>Diario de Vidas es una convocatoria de adolescentes entre 15 y 20 años, de todas las orientaciones sexuales y expresiones de género, a contar relatos de vida que tengan relación a su identidad sexual, en 200 palabras. Junto con estos relatos, se invita a recrear el día a día de quienes participen a través de videos de 10 segundos y fotografías relevantes a los relatos. Los relatores pueden ser anónimos y usar pseudónimos para referirse a sí mismos. Los videos no necesariamente deben mostrar la cara de quien relata, sino que su entorno y contexto.</p>
-		</div>
-		<div>
-			<p class="significant align_right">:Ejemplo de foto</p>
-			<img src="<?php bloginfo('template_url') ?>/assets/images/photo_example.png" alt="">
-		</div>
-		<div>
-			<p class="significant">Ejemplo de video:</p>
-			<img src="<?php bloginfo('template_url') ?>/assets/images/vid_example.png" alt="">
-		</div>
-		<div>
-			<p>Estos relatos pasarán luego por una etapa de selección por parte de nuestro equipo editor, el cual luego generará un video que represente estos relatos, el cual quedará en la sección “Galería de relatos”, con el fin de generar conciencia sobre la diversidad sexual lo que los jóvenes viven alrededor de su sexualidad.</p>
-		</div>
-	</section>
-	<section class="relato_example">
-		<p>El siguiente es un ejemplo de relato real enviado anónimamente:</p>
-		<p class="significant">“Llámame Alex. ¿Por qué? Simple. Me gusta ese nombre. Iba en un colegio cristiano y desde los siete años que me di cuenta de mi homosexualidad. A esa edad tenía una pequeña atracción con los hombres que con el tiempo fue aumentando. A esa tierna edad, me comenzaron a gustar los hombres mayores que yo —los chicos que iban en cursos de la media, por ejemplo—. A los 11 años, mis compañeros dejaron de repudiar a las chicas, y cada broma entre hombres era motivo de burla con las palabras de “maricón”, “gay”, “¿te gustan los hombres acaso?”, “sé hombre”, etc.. Por esto guardé mi orientación sexual como un secreto, algo que nadie debía saber y que era malo. Pensé que, si alguna vez se enteraban me humillaron y tratarán como alguien inferior a ellos.. Pasaron los años hasta que ingresé a la educación media. Mi familia notó cómo pasé de ser un niño risueño y curioso a un chico serio y tímido que incluso tartamudeaba a la hora de hablar. En esa fecha todavía no se enteraban de que yo era gay. Con el paso del tiempo, mi profesora jefe decidió cambiarse de puesto. Lo que fue el mejor camino que pudo tomar en mi homosexual vida. La persona que se sentaba al lado mío actualmente es mi mejor amiga. Violet y yo somos totalmente contrarios y las bromas siguieron existiendo, sí, pero eran sólo eso, simples bromas. Mi situación era entendida. Y eso, era precioso. Ya no me sentía distinto. Me sentía incluido, uno más de mis compañeros. Y quizá no lo sepan ahora y nunca lo hagan, pero se los agradezco de corazón.”</p>
-	</section>
-</main>
-
 <?php get_footer() ?>

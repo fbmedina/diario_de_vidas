204f82a51f2885ca92cb2788aa58017ac306ce7a
Francisca Medina Concha <fbmedina@uc.cl>
2017-11-07 15:44:43 -0300

    section class title and the header in header section

--- wp-content/themes/diario_vidas/page.php
+++ wp-content/themes/diario_vidas/page.php
@@ -1,17 +1,9 @@
 <?php get_header() ?>
 <?php the_post() ?>
-<main>
-	<section class="landing">
-		<h1>Diario de Vidas</h1>
-		<div class="main_description">
-			<p>Autobiografía y relatos de vida para el apoyo a la diversidad sexual adolescente.</p>
-		</div>
-	</section>
-	<section class="remarkable">
-		<h1>Diario de Vidas</h1>
+	<section class="title">
+		<h2><?php the_title() ?></h2>
 	</section>
 	<section class="content">
-		<h2><?php the_title() ?></h2>
 		<?php the_content(); ?>
 	</section>
 </main>

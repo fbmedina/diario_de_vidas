26a06a905f3f99f85fec12686be8beb244f1347f
Francisca Medina Concha <fbmedina@uc.cl>
2017-11-08 11:07:37 -0300

    single member

---
+++ wp-content/themes/diario_vidas/single-member.php
@@ -0,0 +1,26 @@
+<?php get_header() ?>
+
+	<?php if ( have_posts() ) { ?>
+		<section class="full">
+			<?php while ( have_posts() ) { ?>
+				<?php the_post(); ?>
+
+				<article class="miembro">
+			     	<div class="image">
+			     		<?php the_post_thumbnail('member'); ?>
+			     	</div>
+
+			     	<div class="description">
+			     		<h3 class="member_name"><?php the_title() ?></h3>
+			     		<h4 class="rol"><?php echo get_post_meta($post->ID, 'Rol', true); ?></h4>
+			     		<p><?php the_content() ?></p>
+			     	</div>
+			     </article>
+
+			<?php } ?>
+		</section>
+	<?php } else { ?>
+		<!-- Content -->
+	<?php } wp_reset_query(); ?>
+</main>
+<?php get_footer() ?>

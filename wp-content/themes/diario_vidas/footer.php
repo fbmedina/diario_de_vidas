570a4f4e97a2f75c2188f2c2c3635f63ce69a946
Francisca Medina Concha <fbmedina@uc.cl>
2017-11-08 00:44:39 -0300

    footer menu

--- wp-content/themes/diario_vidas/footer.php
+++ wp-content/themes/diario_vidas/footer.php
@@ -1,5 +1,8 @@
 <?php if ( has_nav_menu( 'footer-menu' ) ) { ?>
-	<?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container_class' => 'header_menu' ) ); ?>
+	<nav class="footer">
+		<?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container_class' => 'footer_menu' ) ); ?>
+		<p class="centered big"><i class="fa fa-bars" aria-hidden="true"></i></p>
+	</nav>
 <?php } ?>
 
 <?php wp_footer() ?>

4c77c85d08da18e39cb11edca5083e17a0b9352f
Francisca Medina Concha <fbmedina@uc.cl>
2017-11-08 11:07:51 -0300

    other pages with structure

--- wp-content/themes/diario_vidas/404.php
+++ wp-content/themes/diario_vidas/404.php
@@ -1,6 +1,8 @@
 <?php get_header() ?>
-
-	<h1>Error 404:</h1>
-	<h2>Página no encontrada</h2>
+	<section class="full">
+		<h1>Error 404:</h1>
+		<h2>Página no encontrada</h2>
+	</section>
+</main>
 
 <?php get_footer() ?>
--- wp-content/themes/diario_vidas/search.php
+++ wp-content/themes/diario_vidas/search.php
@@ -1,24 +1,27 @@
 <?php get_header() ?>
 
 <?php if ( have_posts() ) { ?>
-	
-	<ul>
-	<?php while ( have_posts() ) { ?>
-		<?php the_post(); ?>
-		<li>
-			<a href="<?php the_permalink() ?>">
-				<?php the_post_thumbnail() ?>
-				<?php the_title() ?>
-				<time datetime="<?php the_time('Y-m-d') ?>"><?php the_time('d \d\e F \d\e Y') ?></time>
-				<?php the_excerpt(); ?>
-			</a>
-		</li>
-	<?php } ?>
-	</ul>
+	<section class="full">
+		<ul>
+		<?php while ( have_posts() ) { ?>
+			<?php the_post(); ?>
+			<li>
+				<a href="<?php the_permalink() ?>">
+					<?php the_post_thumbnail() ?>
+					<?php the_title() ?>
+					<time datetime="<?php the_time('Y-m-d') ?>"><?php the_time('d \d\e F \d\e Y') ?></time>
+					<?php the_excerpt(); ?>
+				</a>
+			</li>
+		<?php } ?>
+		</ul>
+	</section>
 
 <?php } else { ?>
-	<p>No hay resultados</p>
+	<section class="full">
+		<p>No hay resultados</p>
+	</section>
 <?php } wp_reset_query(); ?>
 
-	<?php get_sidebar() ?>
+</main>
 <?php get_footer() ?>

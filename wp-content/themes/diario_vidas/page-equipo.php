ae2688c160517ef105eeea6213186ff77d386c2f
Francisca Medina Concha <fbmedina@uc.cl>
2017-11-09 19:52:53 -0300

    mail formequipo member

--- wp-content/themes/diario_vidas/page-equipo.php
+++ wp-content/themes/diario_vidas/page-equipo.php
@@ -23,6 +23,7 @@
 			     		<h3 class="member_name"><?php the_title() ?></h3>
 			     		<h4 class="rol"><?php echo get_post_meta($post->ID, 'Rol', true); ?></h4>
 			     		<p><?php the_content() ?></p>
+			     		<p class="align_right"><a href="mailto:<?php echo get_post_meta($post->ID, 'mail', true); ?>"><?php echo get_post_meta($post->ID, 'mail', true); ?></a></p>
 			     	</div>
 			     </article>
 		    <?php } wp_reset_postdata();

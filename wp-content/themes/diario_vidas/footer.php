72803ffd0104d6c521fcd57f50d1da44cf260f50
Francisca Medina Concha <fbmedina@uc.cl>
2017-11-14 21:21:58 -0300

    no hamburger in footer

--- wp-content/themes/diario_vidas/footer.php
+++ wp-content/themes/diario_vidas/footer.php
@@ -1,7 +1,6 @@
 <?php if ( has_nav_menu( 'footer-menu' ) ) { ?>
 	<nav class="footer">
 		<?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container_class' => 'footer_menu' ) ); ?>
-		<p class="centered bigger"><i class="fa fa-bars" aria-hidden="true"></i></p>
 	</nav>
 <?php } ?>
 

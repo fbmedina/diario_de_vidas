d2cccb7c2da6242465d26fd22d8688bb79b172cf
Francisca Medina Concha <fbmedina@uc.cl>
2017-11-16 04:10:26 -0300

    jquery form instagram feed

---
+++ wp-content/themes/diario_vidas/page-instagram.php
@@ -0,0 +1,11 @@
+<?php get_header() ?>
+<?php the_post() ?>
+
+	<section class="full">
+		<?php the_content(); ?>
+	</section>
+</main>
+
+<script   src="https://code.jquery.com/jquery-1.12.4.min.js"   integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ="   crossorigin="anonymous"></script>
+
+<?php get_footer() ?>

1fe4446a099c09b1a507b6e46e312c442fead194
Francisca Medina Concha <fbmedina@uc.cl>
2017-11-15 21:04:11 -0300

    single style

--- wp-content/themes/diario_vidas/functions/wp_enqueue_style.php
+++ wp-content/themes/diario_vidas/functions/wp_enqueue_style.php
@@ -6,15 +6,14 @@
 		/* Register Scripts */
 		wp_register_style('reset', get_parent_theme_file_uri('/assets/css/reset.css'), null, $theme_data->get( 'Version' ), 'screen');
 		wp_register_style('mainStyle', get_parent_theme_file_uri('/assets/css/main.css'), array('reset'), $theme_data->get( 'Version' ), 'screen');
-		wp_register_style('extraSmallScreen', get_theme_file_uri() . '/assets/css/screen-xs.css', null, null, null);
-		wp_register_style('smallScreen', get_theme_file_uri() . '/assets/css/screen-sm.css', array('extraSmallScreen'), null, null);
-		wp_register_style('mediumScreen', get_theme_file_uri() . '/assets/css/screen-md.css', array('smallScreen'), null, null);
-		wp_register_style('largeScreen', get_theme_file_uri() . '/assets/css/screen-lg.css', array('mediumScreen'), null, null);
-		wp_register_style('extraLargeScreen', get_theme_file_uri() . '/assets/css/screen-xl.css', array('largeScreen'), null, null);
+		wp_register_style('extraSmallScreen', get_theme_file_uri() . '/assets/css/screen-xs.css', array('mainStyle'), $theme_data->get( 'Version' ), 'screen');
+		wp_register_style('smallScreen', get_theme_file_uri() . '/assets/css/screen-sm.css', array('extraSmallScreen'), $theme_data->get( 'Version' ), 'screen');
+		wp_register_style('mediumScreen', get_theme_file_uri() . '/assets/css/screen-md.css', array('smallScreen'), $theme_data->get( 'Version' ), 'screen');
+		wp_register_style('largeScreen', get_theme_file_uri() . '/assets/css/screen-lg.css', array('mediumScreen'), $theme_data->get( 'Version' ), 'screen');
+		wp_register_style('xtraLargeScreen', get_theme_file_uri() . '/assets/css/screen-xl.css', array('largeScreen'), $theme_data->get( 'Version' ), 'screen');
 		
 		/* Enqueue Scripts */
-		wp_enqueue_style('mainStyle');
-		wp_enqueue_style('extraLargeScreen');
+		wp_enqueue_style('xtraLargeScreen');
 	}
 
 	add_action( 'wp_enqueue_scripts', 'dl_enqueue_style' );
